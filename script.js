dba82539b16acb03b9a60e52e173254da6a9d594
secretive20 <2fortnite2lol@gmail.com>
2025-11-05 11:55:40 +0200

    Finish

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const currenInput = document.getElementsByClassName('curren-input')
 const currenOutput = document.getElementsByClassName('curren-output')
+let input = document.getElementById('input')
+let output = document.getElementById('output')
 let currenInputValue = ''
 let currenOutputValue = ''
 
@@ -20,3 +22,26 @@ for (let btn of currenOutput) {
         currenOutputValue = event.target.innerText
     })
 }
+
+async function convert() {
+    const resp = await fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+    const data = await resp.json()
+    let usd = data.find(item => item.cc === 'USD').rate
+    let eur = data.find(item => item.cc === 'EUR').rate
+    let uah = 1
+    input.addEventListener('input', () => {
+        if (currenInputValue === 'UAH₴' && currenOutputValue === 'EUR€') {
+            output.value = input.value / eur
+        } else if (currenInputValue === 'UAH₴' && currenOutputValue === 'USD$') {
+            output.value = input.value / usd
+        } else if (currenInputValue === 'EUR€' && currenOutputValue === 'UAH₴') {
+            output.value = input.value * eur
+        } else if (currenInputValue === 'USD$' && currenOutputValue === 'UAH₴') {
+            output.value = input.value * usd
+        } else if (currenInputValue === 'UAH₴' && currenOutputValue === 'UAH₴') {
+            output.value = input.value
+        }
+    })
+}
+
+convert();
